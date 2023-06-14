20ac548398803caaeb3f8828d03bac8120e4cf20
Felipe Fransa <antoniofelipesilva@live.com>
2023-06-14 18:34:51 -0300

    add routes 1.0

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response )=>{
+app.get('/', (req: Request, res: Response ) =>{
     res.send('Servidor Online!')
+})
+
+app.get('/login/:slug', (req: Request, res: Response) =>{
+    let slug = req.params.slug;
+    res.send(`Bem vindo ${slug}`)
+})
+app.get('/register', (req: Request, res: Response) => {
+    res.send("Pagina de cadastro!")
 })
 
 app.listen(3000, ()=> {console.log('Servidor Online!')})
